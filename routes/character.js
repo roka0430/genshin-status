27bdb11d4a2963e62ed5f1b6f1797cd25fa62f18
Roka0430 <noxx0430@gmail.com>
2026-07-19 09:28:38 +0900

    feat: キャラクタid一覧取得及びキャラクタid検索用のapiを作成

---
+++ routes/character.js
@@ -0,0 +1,33 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import * as yaml from "js-yaml";
+import { resourceUsage } from "process";
+
+const router = express.Router();
+
+const __filename = import.meta.filename;
+const __dirname = import.meta.dirname;
+
+const characters = yaml.load(fs.readFileSync(path.join(__dirname, "../data/character.yaml"), "utf-8"));
+
+router.get("/", (req, res) => {
+  res.json(Object.keys(characters));
+});
+
+router.get("/:id", (req, res) => {
+  const character = characters[req.params.id];
+
+  if (!character) {
+    return res.status(404).json({
+      error: "Not found",
+    });
+  }
+
+  res.json({
+    id: req.params.id,
+    ...character,
+  });
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import characterRouter from "./routes/character.js";
 
 const __filename = import.meta.filename;
 const __dirname = import.meta.dirname;
@@ -8,5 +9,6 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/api/character", characterRouter);
 
 app.listen(PORT, () => console.log(`Server: http://localhost:${PORT}`));
