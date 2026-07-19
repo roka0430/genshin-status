332a3fe6bf8cd9eac924082f566968447e0ac8a2
Roka0430 <noxx0430@gmail.com>
2026-07-20 04:44:50 +0900

    refactor: 静的HTMLからEJSへ移行

--- server.js
+++ server.js
@@ -8,8 +8,13 @@ const __dirname = import.meta.dirname;
 const app = express();
 const PORT = 3001;
 
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/character", characterRouter);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(PORT, () => console.log(`Server: http://localhost:${PORT}`));
