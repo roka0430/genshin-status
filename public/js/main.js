3156e17e925ae3e5e307ef8f05836bbb58bcafeb
Roka0430 <noxx0430@gmail.com>
2026-07-22 08:47:37 +0900

    静的JSにスケルトンコードを追加

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+class ApiClient {
+  constructor() {}
+
+  async getCharacters() {}
+
+  async getCharacter(id) {}
+
+  async getWeapons() {}
+
+  async getWeapon() {}
+
+  async getPresets() {}
+}
+
+class CharacterManager {
+  constructor() {}
+
+  async init() {}
+
+  async getDetail(id) {}
+}
+
+class WeaponData {
+  constructor(api) {}
+
+  async init() {}
+
+  getByType(type) {}
+
+  get(id) {}
+}
+
+class Application {
+  constructor() {}
+}
